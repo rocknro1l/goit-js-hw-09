cb48f9887349b472bfcca91949dd21adaf88b1e1
rocknro1l <alexst383@gmail.com>
2023-02-08 11:21:52 +0100

    finish

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const stop = document.querySelector('[data-stop]');
+const start = document.querySelector('[data-start]');
 
+start.addEventListener('click', onStartPress);
+stop.addEventListener('click', onStopPress);
+
+const INTERVAL_DURATION = 1000;
+
+function onStartPress(event) {
+  bodyColor = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, INTERVAL_DURATION);
+
+  start.disabled = true;
+  stop.disabled = false;
+}
+
+function onStopPress(event) {
+  clearInterval(bodyColor);
+  start.disabled = false;
+  stop.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
